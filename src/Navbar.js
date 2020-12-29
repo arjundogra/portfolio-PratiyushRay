1305eb6d6a99f794931d6508851c709223de7da0
Arjun Dogra <arjundogra146@gmail.com>
2020-12-29 09:41:58 +0530

    first

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>Pratiyush Ray</h2>
+      <div className="navbar_nav">
+        <div className="navbar_options">Home</div>
+        <div className="navbar_options">About</div>
+        <div className="navbar_options">Experience</div>
+        <div className="navbar_options">Resume</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
