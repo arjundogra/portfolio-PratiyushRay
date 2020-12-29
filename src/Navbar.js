eca82850b6f0169be96afeba98187c9557f0a784
Arjun Dogra <arjundogra146@gmail.com>
2020-12-29 13:41:30 +0530

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,12 +3,17 @@ import React from "react";
 function Navbar() {
   return (
     <div className="navbar">
-      <h2>Pratiyush Ray</h2>
       <div className="navbar_nav">
         <div className="navbar_options">Home</div>
-        <div className="navbar_options">About</div>
-        <div className="navbar_options">Experience</div>
-        <div className="navbar_options">Resume</div>
+      </div>
+      <div className="navbar_nav">
+        <div className="navbar_options">Home</div>
+      </div>
+      <div className="navbar_nav">
+        <div className="navbar_options">Home</div>
+      </div>
+      <div className="navbar_nav">
+        <div className="navbar_options">Home</div>
       </div>
     </div>
   );
