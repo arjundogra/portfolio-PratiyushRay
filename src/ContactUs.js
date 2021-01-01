747b23e297165ff6ee37afb864682b7e6b49dab2
Arjun Dogra <arjundogra146@gmail.com>
2021-01-01 16:55:21 +0530

    responsive

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,9 +1,6 @@
 import React from "react";
-import PhoneIcon from "@material-ui/icons/Phone";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
 import EmailIcon from "@material-ui/icons/Email";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
@@ -20,9 +17,6 @@ function ContactUs() {
         RESUME
       </a>
       <div className="icons">
-        <a href="tel:9991234846">
-          <PhoneIcon />
-        </a>
         <a href="mailto:pkray0021@gmail.com">
           <EmailIcon />
         </a>
